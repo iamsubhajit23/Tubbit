8959d57003caa03075f11c9a0660ed0e002de334
Subhajit Dash <dashsubhajit702@gmail.com>
2025-06-26 03:32:40 +0530

    fix: increase autoClose duration for error notifications to improve user experience

--- client/src/utils/notification/error.js
+++ client/src/utils/notification/error.js
@@ -3,7 +3,7 @@ import { toast, Bounce } from "react-toastify";
 const errorToast = (message) => {
   return toast.error(message, {
     position: "bottom-right",
-    autoClose: 1000,
+    autoClose: 3000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
