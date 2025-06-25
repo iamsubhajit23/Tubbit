016d4d52878a6b3997889f8ded56aca9c3dcef7a
Subhajit Dash <dashsubhajit702@gmail.com>
2025-06-26 03:33:54 +0530

    fix: add subscription slice to Redux store configuration

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./AuthSlice.js";
+import subscriptionSlice from "./slices/subscriptionSlice.js"
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
+    subscription: subscriptionSlice,
   },
 });
 
