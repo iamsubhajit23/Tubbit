e4c644aa0196d2db5ea99c3ce42efe2d6d5372b6
Subhajit Dash <dashsubhajit702@gmail.com>
2025-06-27 01:30:59 +0530

    refactor: update import paths. feat: fetch and set liked Videos in app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { Navbar } from "./components/header/Navbar.jsx";
 import { getCurrentUser } from "./services/user/profile.api.js";
-import { login, logout } from "./store/AuthSlice.js";
+import { login, logout } from "./store/slices/AuthSlice.js";
 import { getSubscribedChannel } from "./services/subscription/subscription.api.js";
 import { setSubscribedChannels } from "./store/slices/subscriptionSlice.js";
+import { getLikedVideos } from "./services/like/like.api.js";
+import { setLikedVideos } from "./store/slices/likeSlice.js";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -15,11 +17,11 @@ const App = () => {
     const checkAuth = async () => {
       const response = await getCurrentUser();
       if ([200, 201].includes(response.statuscode)) {
-        dispatch(login({userData: response}));
-      }else{
-        dispatch(logout())
+        dispatch(login({ userData: response }));
+      } else {
+        dispatch(logout());
       }
-    }
+    };
 
     const fetchSubscriptions = async () => {
       const res = await getSubscribedChannel();
@@ -29,9 +31,18 @@ const App = () => {
       }
     };
 
+    const fetchLikedVideos = async () => {
+      const res = await getLikedVideos();
+      if (res.statuscode === 200) {
+        const ids = res.data.likedVideos.map((v) => v._id);
+        dispatch(setLikedVideos(ids));
+      }
+    };
+
     checkAuth();
     fetchSubscriptions();
-  }, [])
+    fetchLikedVideos();
+  }, []);
 
   return (
     <>
