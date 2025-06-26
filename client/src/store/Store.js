ab0d269b9fdf405c892fffee66b7b6f1412f3e56
Subhajit Dash <dashsubhajit702@gmail.com>
2025-06-27 01:26:20 +0530

    feat: add like slice to Redux store configuration

--- client/src/store/Store.js
+++ client/src/store/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from "./AuthSlice.js";
-import subscriptionSlice from "./slices/subscriptionSlice.js"
+import authSlice from "./slices/AuthSlice.js";
+import subscriptionSlice from "./slices/subscriptionSlice.js";
+import likeSlice from "./slices/likeSlice.js";
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
     subscription: subscriptionSlice,
+    like: likeSlice,
   },
 });
 
