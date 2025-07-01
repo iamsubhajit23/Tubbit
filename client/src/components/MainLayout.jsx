3efab862ce807c88911c8e98c4344cfdfd9760b9
Subhajit Dash <dashsubhajit702@gmail.com>
2025-07-01 12:24:59 +0530

    refactor: simplify MainLayout by removing unnecessary header and adjusting Navbar import

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "./ui/Sidebar.jsx";
 import AppSidebar from "./AppSidebar.jsx";
-import {Navbar} from "./header/Navbar.jsx";
+import { Navbar } from "./header/Navbar.jsx";
 import App from "../App.jsx";
 
 const MainLayout = () => {
@@ -11,12 +11,7 @@ const MainLayout = () => {
       <div className="flex min-h-screen w-full">
         <AppSidebar />
         <div className="flex-1 flex flex-col">
-          <header className="h-15 flex items-center px-4 border-b">
-            <SidebarTrigger />
-            <div className="ml-4 flex-1">
-              <Navbar />
-            </div>
-          </header>
+          <Navbar />
           <main className="flex-1">
             <Outlet />
           </main>
