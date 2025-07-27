77e766d4cf25ce35f6fb1f8e6a6a65e261ccab36
Subhajit Dash <154728324+iamsubhajit23@users.noreply.github.com>
2025-07-28 03:39:37 +0530

    feat: add tweet deletion and editing functionality with dropdown menu

--- client/src/components/TweetCard.jsx
+++ client/src/components/TweetCard.jsx
@@ -1,15 +1,24 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   Heart,
   MessageCircle,
   Repeat,
   Share,
   MoreHorizontal,
+  Trash2,
+  Edit2,
+  Edit,
 } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Avatar, AvatarImage, AvatarFallback } from "./ui/Avatar.jsx";
 import { Card } from "./ui/Card.jsx";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "./ui/DropDownMenu.jsx";
 import { Button } from "./ui/Button.jsx";
 import errorToast from "../utils/notification/error.js";
 import {
@@ -17,6 +26,8 @@ import {
   toggleLikeOnTweet,
 } from "../services/like/like.api.js";
 import { getTweetComments } from "../services/comment/comment.api.js";
+import { deleteTweet, getTweetById } from "../services/tweet/tweet.api.js";
+import { formatDistanceToNow } from "date-fns";
 
 const TweetCard = ({
   tweetId,
@@ -24,15 +35,57 @@ const TweetCard = ({
   username,
   fullname,
   userAvatar,
-  timestamp,
+  updatedAt,
   images = [],
 }) => {
   const imageList = images ? (Array.isArray(images) ? images : [images]) : [];
   const [isLiked, setIsLiked] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
   const [repliesCount, setRepliesCount] = useState(0);
+  const [tweetOwnerId, setTweetOwnerId] = useState(null);
+  const [isTweetDeleting, setIsTweetDeleting] = useState(false);
+
   const navigate = useNavigate();
   const authStatus = useSelector((state) => state.auth.status);
+  const authUserData = useSelector((state) => state.auth.userData);
+
+  const timestamp = formatDistanceToNow(new Date(updatedAt), {
+    addSuffix: true,
+  });
+
+  useEffect(() => {
+    const fetchTweetOwnerId = async () => {
+      const res = await getTweetById(tweetId);
+      if (res?.statuscode === 200) {
+        setTweetOwnerId(res?.data?.tweet?.owner?._id);
+      } else {
+        return;
+      }
+    };
+    fetchTweetOwnerId();
+  }, [tweetId]);
+
+  const isOwner = authUserData?.data?._id === tweetOwnerId;
+
+  const handleTweetDeleting = async () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this tweet?"
+    );
+    if (!confirmed) return;
+
+    setIsTweetDeleting(true);
+    const res = await deleteTweet(tweetId);
+
+    if (res?.statuscode === 200) {
+      successToast("Tweet deleted successfully");
+      window.location.reload();
+    } else {
+      errorToast("Failed to delete tweet");
+    }
+    setIsTweetDeleting(false);
+  };
+
+  const handleTweetEditing = async () => {};
 
   useEffect(() => {
     const fetchTweetLikes = async () => {
@@ -87,11 +140,8 @@ const TweetCard = ({
       <div className="flex gap-3">
         <Avatar
           onClick={() => {
-                authStatus
-                  ? navigate(`/profile/${username}`)
-                  : navigate("/auth");
-              }}
-          className="w-10 h-10 cursor-pointer"
+            authStatus ? navigate(`/profile/${username}`) : navigate("/auth");
+          }}
         >
           <AvatarImage src={userAvatar} alt={username} />
           <AvatarFallback>{fullname?.slice(0, 1).toUpperCase()}</AvatarFallback>
@@ -101,11 +151,7 @@ const TweetCard = ({
           {/* Header */}
           <div className="flex items-center gap-2 mb-1">
             <span
-              onClick={() => {
-                authStatus
-                  ? navigate(`/profile/${username}`)
-                  : navigate("/auth");
-              }}
+              onClick={() => navigate(`/profile/${username}`)}
               className="font-semibold hover:underline cursor-pointer"
             >
               {fullname}
@@ -116,11 +162,29 @@ const TweetCard = ({
             <span className="text-muted-foreground">•</span>
             <span className="text-muted-foreground text-sm">{timestamp}</span>
 
-            <div className="ml-auto">
-              <Button variant="ghost" size="sm" className="h-8 w-8 p-0">
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </div>
+            {isOwner && (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div className="ml-auto">
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="h-8 w-8 p-0 text-muted-foreground hover:bg-[#020816] hover:text-foreground"
+                    >
+                      <MoreHorizontal className="h-5 w-5" />
+                    </Button>
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="end">
+                  <DropdownMenuItem onClick={handleTweetDeleting}>
+                    <Trash2 className="w-4 h-4 mr-2" /> Delete Tweet
+                  </DropdownMenuItem>
+                  <DropdownMenuItem onClick={handleTweetEditing}>
+                    <Edit className="w-4 h-4 mr-2" /> Edit Tweet
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
           </div>
 
           {/* Content */}
@@ -193,5 +257,4 @@ const TweetCard = ({
     </Card>
   );
 };
-
-export default TweetCard;
+export default React.memo(TweetCard);
